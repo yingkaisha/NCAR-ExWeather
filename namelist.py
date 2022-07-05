1af8f0313ab85a7f7ce1b43b53e495f568b975e3
Yingkai Sha <yingkaisha@gmail.com>
2022-07-05 15:41:27 -0600

    update namelist

---
+++ namelist.py
@@ -0,0 +1,5 @@
+data_dir = '/glade/work/ksha/NCAR/'
+report_dir = data_dir+'storm_report/'
+NCAR500_dir = '/glade/p/mmm/parc/sobash/NSC/3KM_WRF_POST_12sec_ts/'
+
+save_dir = '/glade/work/ksha/NCAR/'
