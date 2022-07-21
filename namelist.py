5b6b7e28e49bc6f2dd08ceb3912ffe13f0d7ef9d
Yingkai Sha <yingkaisha@gmail.com>
2022-07-21 14:27:54 -0600

    update config files

--- namelist.py
+++ namelist.py
@@ -1,5 +1,11 @@
 data_dir = '/glade/work/ksha/NCAR/'
 report_dir = data_dir+'storm_report/'
 NCAR500_dir = '/glade/p/mmm/parc/sobash/NSC/3KM_WRF_POST_12sec_ts/'
-
+HRRR_dir = '/glade/scratch/ksha/DRIVE/HRRR/'
 save_dir = '/glade/work/ksha/NCAR/'
+save_dir_scratch = '/glade/scratch/ksha/DRIVE/'
+
+
+data_pd500_dir = '/glade/scratch/ksha/DRIVE/clean_NCAR/'
+data_pdhrrr_dir = '/glade/scratch/ksha/DRIVE/clean_HRRR/'
+
