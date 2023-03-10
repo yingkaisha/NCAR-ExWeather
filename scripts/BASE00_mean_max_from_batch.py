5555b38b71efd876d54bd2ece07741d8cfd9eed3
Yingkai (Kyle) Sha <yingkaisha@gmail.com>
2023-03-10 09:35:56 -0700

    Update BASE00_mean_max_from_batch.py

--- scripts/BASE00_mean_max_from_batch.py
+++ scripts/BASE00_mean_max_from_batch.py
@@ -1,15 +1,18 @@
+'''
+Compute the max and mean values from each 64-by-64 training batch and all its predictors.
+The max and mean values are applied to the development of an MLP baseline
+'''
+
+
 # general tools
 import os
 import re
 import sys
 import time
-import h5py
-import random
+import numpy as np
 from glob import glob
 
-import numpy as np
 from datetime import datetime, timedelta
-from random import shuffle
 
 sys.path.insert(0, '/glade/u/home/ksha/NCAR/')
 sys.path.insert(0, '/glade/u/home/ksha/NCAR/libs/')
@@ -20,35 +23,38 @@ import data_utils as du
 import argparse
 
 parser = argparse.ArgumentParser()
-
 parser.add_argument('lead', help='lead')
-
 args = vars(parser.parse_args())
 
 # ===================================== #
+# obtain the current forecast lead time
 lead = int(args['lead'])
 
+# ===================================== #
+# File path
 filepath_vec = "/glade/work/ksha/NCAR/"
-
+# HRRR v3
 path_name1_v3 = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v3/'
 path_name2_v3 = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v3/' 
 path_name3_v3 = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v3/'
 path_name4_v3 = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v3/'
+# HRRR v4x
+# path_name1_v4 = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4x/'
+# path_name2_v4 = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4x/'
+# path_name3_v4 = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4x/'
+# path_name4_v4 = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4x/'
+# HRRR v4
+# path_name1_v4_test = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4_temp/'
+# path_name2_v4_test = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4_temp/'
+# path_name3_v4_test = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4_temp/'
+# path_name4_v4_test = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4_temp/'
 
-path_name1_v4 = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4x/'
-path_name2_v4 = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4x/'
-path_name3_v4 = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4x/'
-path_name4_v4 = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4x/'
-
-path_name1_v4_test = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4_temp/'
-path_name2_v4_test = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4_temp/'
-path_name3_v4_test = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4_temp/'
-path_name4_v4_test = '/glade/campaign/cisl/aiml/ksha/NCAR_batch_v4_temp/'
-
+# Pull file sequence from HRRR v3 path
 filename_train_lead_v3 = sorted(glob("{}TRAIN*lead{}.npy".format(path_name1_v3, lead)))
 filename_valid_lead_v3 = sorted(glob("{}VALID*lead{}.npy".format(path_name1_v3, lead)))
 filename_test_lead = sorted(glob("{}TEST*lead{}.npy".format(path_name1_v4_test, lead)))
 
+# Estimate the total number of batch files
 L_train = len(filename_train_lead_v3)
 L_valid = len(filename_valid_lead_v3)
 L_test = len(filename_test_lead)
